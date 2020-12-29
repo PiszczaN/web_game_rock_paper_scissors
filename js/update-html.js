75d62c64de7f00fd49a613a1df45cdcd83b4f077
Jakub Piszczan <jpiszczan@gmail.com>
2020-12-29 18:18:03 +0100

    back to match view

--- js/update-html.js
+++ js/update-html.js
@@ -1,9 +1,18 @@
+import { GamePawn } from "./enums.js";
+import { Options } from "./options.js";
 export class UpdateHtml {
+
+    constructor() {
+        this.paperPawn = new GamePawn(1, '<div class="gameBoardItem__image">papier</div>');
+        this.stonePawn = new GamePawn(2, '<div class="gameBoardItem__image">kamien</div>');
+        this.scissorsPawn = new GamePawn(3, '<div class="gameBoardItem__image">nozyce</div>');
+    }
 
     static winScore = 0;
     static drawScore = 0;
     static failScore = 0;
     static roundCounter = 1;
+    static board = document.querySelector(".gameBoard");
 
     updateScoreTable(result) {
         const wins = document.querySelector(".wins");
@@ -25,10 +34,20 @@ export class UpdateHtml {
         }
     }
     matchResultView(result, player, computer) {
-        const board = document.querySelector(".gameBoard");
-        const gameBoard = document.querySelector(".game__board");
 
+        UpdateHtml.board.innerHTML = `<div class="gameBoard__item">${player}</div><div class="gameBoard__item">${computer}<button class="gameBoard__matchResult">Graj dalej!</button></div>`;
+        const BackToMatchView = document.querySelector(".gameBoard__matchResult");
+        BackToMatchView.addEventListener("click", () => { this.matchView() });
+    }
+    matchView() {
+        UpdateHtml.board.innerHTML = `<div class="gameBoard__item">${this.paperPawn.html}
+            <div class = "gameBoardItem__icon"> X </div></div>
+            <div class = "gameBoard__item">${this.stonePawn.html}
+            <div class = "gameBoardItem__icon" > Y </div></div>
+            <div class = "gameBoard__item">${this.scissorsPawn.html}
+            <div class = "gameBoardItem__icon" > Z </div></div>`;
 
-        board.innerHTML = `<div class="gameBoard__item">${player}</div><div class="gameBoard__item">${computer}</div>`;
+        const gameResume = new Options();
+        gameResume.init();
     }
 }
